2519c6c5771fa2179be054223caa1772dd8ab7e6
Al-Fareed <alfareedss472@gmail.com>
2022-07-17 12:34:52 +0530

    Blogging completed

---
+++ src/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>
+                This is an Application for posting Blogs and Articles developed using React JS by Al-Fareed.<br/>
+                Explore the app, Share your thoughts and Blogs. <br/>
+                <div style={{postion:"fixed",marginTop:"50%",display:"flex"}}>
+                For more information mail: alfareedss472@gmail.com
+                </div>
+            </p>
+        </main>
+    )
+}
+
+export default About
